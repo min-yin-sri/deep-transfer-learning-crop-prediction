86dc2ded76a98ad0ce2508fba92d3858aa483793
Min Yin <min.yin@sri.com>
2019-05-29 18:41:38 -0700

    Restore condition

--- code/create_south_sudan_coordinates_csv.py
+++ code/create_south_sudan_coordinates_csv.py
@@ -68,8 +68,7 @@ if __name__ == "__main__":
     if index == 0:
         index = index + 1
         continue
-    #elif (float(entry[3]) >= LAT_MIN and float(entry[3]) <= LAT_MAX 
-    elif (float(entry[4]) >= LON_MIN and float(entry[4]) <= LON_MAX):
+    elif (float(entry[3]) >= LAT_MIN and float(entry[3]) <= LAT_MAX and float(entry[4]) >= LON_MIN and float(entry[4]) <= LON_MAX):
         south_sudan_csv_list.append(entry)
         logging.info("Get a South Sudan csv entry: %s %s %s" % (entry[0], entry[3], entry[4]) )
     index = index + 1
